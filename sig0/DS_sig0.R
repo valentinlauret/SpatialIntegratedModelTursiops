473f410426c1bd026972cb867d169454304a9f3f
Valentin Lauret <valentin.lauret@gmail.com>
2022-03-23 11:29:15 +0100

    add current work

---
+++ sig0/DS_sig0.R
@@ -0,0 +1,195 @@
+# SIPM 1
+
+# Combining Distance sampling & SCR data
+# Distance sampling --> SAMM data
+# SCR --> GDEGeM data
+
+#--- load packages  -----
+library(nimble)
+library(tidyverse)
+library(sf)
+
+# ----function to load data  -----
+
+# DS 
+dataDS <- function(){
+  
+  #load DS datasets
+  load("DS2.Rdata")
+  load("sea.grid.rdata")
+  
+  g2 <- sea
+  
+  #---- DS datasets ----
+  
+  dim(sites) # "sites" contient les donnees sur les 1335 sites echantillonnés par SAMM avec les cov
+  dim(indiv) # "indiv" contient les 129 detections de groupe de dauphins avec les cov
+  
+  # create variables for DS
+  sitesDS <- sites[]
+  nsitesDS <- nrow(sites) # nb de sites prospectes : 1335
+  nobs <- nrow(indiv) # nb of detections : 129 # 
+  
+  site <- indiv$DSid #   site id of every detection
+  
+  # sampled sites by aerial surveys 
+  gDS <- g2 %>% mutate(DSid = 1:nrow(g2))
+  
+  sampDS <- rep(NA, nrow(sites))
+  for(i in 1:nrow(sites)){
+    index <-  sites$objectid[i]
+    sampDS[i] <-  which(g2$objectid == index)
+  }
+  
+  
+  # distance class format
+  d <- indiv$d_perp /max(indiv$d_perp) # distance perpendiculaire au transect. 0< d < 1
+  B <- max(d)
+  delta <- 0.1 # distance bin width, we make 10 bins
+  midpt <- seq(delta/2, B, delta) # make mid-points and chop up data
+  dclass <- d %/% delta + 1 # convert distances to cat. distances
+  nD <- length(midpt) # Number of distance intervals
+  
+  # group size format
+  gp <- sites$taille_grp ## sitesDS$groupsize # taille du groupe detecte
+  groupsize <- gp # Input groupsize as data
+  groupsize[is.na(groupsize)] <- 0
+  
+  
+  # covariates 
+  seffDS <- log(as.numeric(sitesDS$samm.effort)) # sapmpling effort covariate
+  #habitat <- as.numeric(sites$bathy.sc) # depth as habitat covariate
+  sea.stateDS <- as.numeric(scale(sitesDS$seg_subj_num)) # sea state as detection covariate
+  
+  # covariates
+  habitat <- as.numeric(g2$bathy.sc)[sampDS] # cov
+  
+  
+  # inits
+  Nst <- groupsize + 1 
+  Nst[is.na(Nst)] <- 0
+  
+  
+  # save list
+  dataDS <- list(nobs = nobs, B = B, nsitesDS = nsitesDS, midpt = midpt, ## DS
+                 delta = delta, nD = nD, site = site,sampDS= sampDS,
+                 seffDS = seffDS, sea.stateDS = sea.stateDS,
+                 habitat = habitat,dclass= dclass,  groupsize = groupsize, Nst = Nst)
+  
+  return(dataDS)
+}
+
+
+# NIMBLE code 
+# ---- BUGS model -----
+
+DScode <-  nimbleCode({
+  
+  # priors 
+  # priors for the mu-density regression  
+  mu0 ~ dnorm(0,1) # Intercept of mu-density regression
+  mu1 ~ dnorm(0,1) # slope of unique covariate for now
+  # priors for the sigma DS regression
+  alpha0 ~ dnorm(0,1)
+  alpha1 ~ dnorm(0,1)
+  alpha2 ~ dnorm(0,1)
+  # prior on sigma
+  sigma ~ dunif(0,5)
+  
+  # DS
+   
+   for(i in 1:nobs){
+     dclass[i] ~ dcat(fc[site[i],1:nD]) # Part 1 of HM
+   }
+   
+   for(s in 1:nsites){
+     
+     # Construct cell probabilities for nD multinomial cells
+     for(g in 1:nD){ # midpt = mid-point of each cell
+       
+       p[s,g] <- p0[s] * exp(- midpt[g] * midpt[g] / (2*sigma*sigma))
+       
+       pi[s,g] <- delta / B # Probability per interval
+       
+       f[s,g] <- p[s,g] * pi[s,g]
+       
+       fc[s,g] <- f[s,g] / pcap[s]
+     }
+     
+     pcap[s] <- sum(f[s,1:nD]) # Pr(capture): sum of rectangular areas
+     gpsize[s] ~ dpois(pcap[s]*lambda[s])
+     #gpsize[s] ~ dbin(pcap[s], N[s]) # Part 2 of HM
+     N[s] ~ dpois(lambda[s]) # Part 3 of HM
+     log(lambda[s]) <- mu0 + mu1 * habitat[s] # Eq 2 Linear model abundance
+     logit(p0[s])<- alpha0 + alpha1*seff[s]  + alpha2*sea.state[s] # Linear model detection
+   }
+ ##  # Derived parameters
+   #Ntot <- sum(N[1:nsites])
+   EN <-  sum(lambda[1:nsites])     # expected number of individual for the study area
+  # area <- nsites*1*2*B # Unit length == 1, half-width = B
+  # D <- Ntotal/area
+  
+})
+
+
+# ---- load DATA ----
+
+
+datDS <- dataDS()
+
+# RUN ---- 
+
+constants <- list(nobs = datDS$nobs, B = datDS$B, nsites = datDS$nsites,midpt = datDS$midpt,
+                  habitat = datDS$habitat, seff = datDS$seff, sea.state = datDS$sea.state, 
+                  delta = datDS$delta, nD = datDS$nD, site = datDS$site)
+
+data <- list(dclass =datDS$dclass, gpsize = datDS$groupsize)
+
+
+inits <- list(alpha0=0, alpha1=0,alpha2= 0,mu0=0, mu1 = 0, sigma = runif(1,0,200), N = datDS$Nst)
+
+# parralellize 
+# 
+# library(parallel)
+# 
+# this_cluster <- makeCluster(3)
+# 
+# set.seed(32)
+# 
+# DS_para <- function(seed, code, constants, data, inits){
+  
+  library(nimble)
+
+
+# Build model
+#tSIPM1 <- proc.time()
+Rmodel <- nimbleModel(DScode, constants, data, inits)
+Rmodel$initializeInfo()
+Rmodel$calculate() #  -75545.54
+
+# Configure monitors
+conf <- configureMCMC(Rmodel)
+
+conf$printMonitors() # see wht parameters are monitored
+conf$resetMonitors()
+conf$addMonitors("Ntot","mu0", "mu1") # add monitors 
+
+# custom samplers OPTIONNAL
+conf$printSamplers(byType= TRUE)
+
+conf$removeSamplers(target = c("mu0","mu1","alpha0","alpha1","alpha2"))
+#conf$removeSamplers(target = c("mu0","mu1","beta0","beta1"))
+conf$addSampler(target = c("mu0","mu1"), type ="RW_block")
+conf$addSampler(target = c("alpha0","alpha1","alpha2"), type ="RW_block")
+conf$printSamplers(byType= TRUE)
+
+# Build and compile MCMC
+Rmcmc <- buildMCMC(conf)
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Cmodel)
+
+# Run 
+t <- system.time(samples <- runMCMC(Cmcmc, niter = 100000, nburnin = 10000, nchains = 3, samplesAsCodaMCMC = TRUE, setSeed = seed))  ## DT: use runMCMC
+
+save(samples, t, file = "DS_sig0res.rdata")
+
---
+++ sig0/SCR_sig0.R
@@ -0,0 +1,352 @@
+# Spatial capture recapture
+
+# This code run SCR analyze for Bottlenose dolphins in the NW Mediterranean Sea
+# It requires "SCR5.rdata" file
+# Analyzes are run with NIMBLE, please visit https://r-nimble.org/ for information
+
+
+  
+#--- load required packages  -----
+library(nimble)
+library(dplyr)
+
+
+# ---- function to load data  -----
+
+dataSCR <- function(M = double(0)){
+  
+  # load data 
+  load("SCR/SCR5.rdata")
+  
+  
+  y.scr <- y.scr[,samp.sites,] # subset the detection history matrix to sampled sites only
+  
+  
+  # get UTM coordinates of sampled sites and of all sites of the study area 
+  traploc <- cbind(g2$X[samp.sites],g2$Y[samp.sites])
+  sites <- cbind(g2$X,g2$Y)
+  
+  
+  # 
+  ybis <- y.scr
+  
+  
+  nsites <- nrow(sites)  # number of sites
+  # full analysis will be run with nsites = 4356
+  nind <- dim(ybis)[1]  # nb of ind = 927
+  nocc <- dim(ybis)[3]  # nb of occ = 8
+  ntrap <- dim(ybis)[2]  # nb of trap = 327 (i.e. nb of sampled sites)
+  
+  
+  # Data Augmentation 
+  M <- M
+  if(M < nind) stop()
+  
+  
+  ## DT: here's the main change we're going to make
+  ## change the representation of ybis, so instead of a 3-dimensional
+  ## array (individuals x traps x occasions) of 0's and 1's,
+  ## instead it will be a 3-dimensional array of
+  ## (individuals x occasions x trap IDs with captures)
+  ## specifically: for individual i, on occasion t, ybis[i, t, 1:MAX] will contain
+  ## the trap IDs of these captures (with 0's following the final capture trap ID,
+  ## or all 0's if individual i was never captured on occasion t.
+  ## we'll have to set the size MAX correctly, based on the maximum
+  ## number of sightings of any individual
+  
+  MAX <- max(apply(ybis, c(1,3), sum))
+  ybis2 <- array(0, c(nind, nocc, MAX))
+  for(i in 1:nind) {
+    for(t in 1:nocc) {
+      trapIDs <- which(ybis[i,,t] > 0)
+      numSightings <- length(trapIDs)
+      if(numSightings > 0) ybis2[i, t, 1:numSightings] <- trapIDs
+    }
+  }
+  
+  
+  ## DT: new data augmentation using ybis2
+  ## augmented individuals just have all 0's in ybis2
+  ## we'll call this new variable y.da2, to distinguish it
+  
+  y.da2 <- array(0, c(M, nocc, MAX))
+  y.da2[1:nind,,] <- ybis2
+  dim(y.da2)
+  max(y.da2) ## must be < ntrap
+  
+  
+  ## DT: create z initial values using y.da2
+  z <- as.numeric(apply(y.da2, 1, sum) > 0)
+  
+  # draw starting locations from the list of sites
+  # for known individual, take the mean location where they have been seen
+  set.seed(32)   ## DT: moved set.seed() here
+  head(sites)
+  
+  id <- sample(1:nsites, M, replace= TRUE)
+  
+  for(i in 1:nind) {
+    
+    if(z[i] == 1) {
+      siteVector <- as.numeric(y.da2[i,,1:MAX])
+      if(max(siteVector) == 0) stop()
+      sitesSeen <- siteVector[which(siteVector>0)]
+      id[i] <- samp.sites[median(sitesSeen)]   
+      ## if an ind is seen at multiple locations, we kept oen location as the starting activity center
+    }
+  }
+  
+  # environmental covariate
+  habitat <- as.numeric(g2$bathy.sc)
+  
+  # effort covariate log-transformed
+  eff.cov <- seff[samp.sites,] 
+  eff.cov <- eff.cov[,c("occ1", "occ2","occ3","occ4", "occ5", "occ6", "occ7", "occ8")]
+  eff.cov$geometry <-  NULL
+  eff.cov <- log(eff.cov)
+  eff.cov <- as.matrix(eff.cov)
+  
+   # as_tibble() %>% 
+   # select(occ1, occ2,occ3,occ4, occ5, occ6, occ7, occ8) %>%
+   # log() %>% 
+   # as.matrix()
+  
+  eff.cov[eff.cov == - Inf] <-  0
+  
+  # make a binary cov with 1 if sites i is sampled during occ j, and 0 otherwise
+  eff.ind <- eff.cov
+  eff.ind[eff.ind!=0] <- 1
+  
+  
+  dataSCR <- list(nsites = nsites, nind = nind, nocc = nocc, ntrap = ntrap, traploc = traploc,
+                  habitat = habitat, seff = eff.cov, eff.ind = eff.ind, MAX = MAX, M = M, sites = sites, z=z,
+                  y.scr = y.da2, id = id)
+  
+  return(dataSCR)
+}
+
+# --- load data ---
+
+M = 5000 # data augmentation parameter
+datSCR <- dataSCR(M = M)
+
+# --- NIMBLE model ----
+SCRcode <-  nimbleCode({
+  
+  # priors for the density regression
+  mu0 ~ dnorm(0,1) # Intercept of mu-density regression
+  mu1 ~ dnorm(0,1) # slope of unique covariate for now
+  # priors for the sigma 
+  sigma ~ dunif(10^2,10^5)
+  sig2 <- 2*sigma*sigma
+  # priors for the pbar regression
+  p0 ~ dnorm(0,1) 
+  p1 ~ dnorm(0,1) 
+  # prior psi
+  #psi ~ dunif(0,1)
+  
+  
+  log(mu[1:nsites]) <- mu0 + mu1 * habitat[1:nsites]  # abundance model via IPP 
+ 
+  EN <- sum(mu[1:nsites]) # expected number of individual for the study area
+  
+  psi <- EN/M
+  
+ 
+  # logit regression of pbar
+  logit(pbar[1:ntrap, 1:nocc]) <- p0 + p1 * seff[1:ntrap,1:nocc] 
+  
+  # cell prob
+  probs[1:nsites] <- mu[1:nsites]/EN
+  
+  # data augmentation
+  for(i in 1:M) {
+    
+    z[i] ~ dbern(psi)  # is i alive or not ?
+    
+    # induce the uniform distribution of activity centers over the discrete set 1:nsites
+    id[i] ~ dunif(0.5, nsites+0.5) 
+    
+    # location of AC
+    SX[i] <- sites[round(id[i]),1]
+    SY[i] <- sites[round(id[i]),2]
+
+    #zeros's trick
+    nll[i] <- - log(probs[round(id[i])])
+    zeros[i] ~ dpois(nll[i])
+    
+    ## DT: likelihood using new dBernoulliVector4 distribution:
+    y.scr[i, 1:nocc, 1:MAX] ~ dBernoulliVector4(
+      MAX = MAX, ntrap = ntrap, nocc = nocc,
+      sigma = sig2, pbar = pbar[1:ntrap,1:nocc], 
+      eff.ind = eff.ind[1:ntrap, 1:nocc], SX = SX[i], SY = SY[i],
+      traploc = traploc[1:ntrap,1:2], indicator = z[i])
+    
+  }
+  N <- sum(z[1:M]) 
+})
+
+# ---- Nimble custom distribution ----
+dBernoulliVector4 <- nimbleFunction(
+  run = function(
+    x = double(2), MAX = double(0), ntrap = double(0), nocc = double(0),
+    sigma = double(0), pbar = double(2), eff.ind = double(2),  SX = double(0), SY = double(0),
+    traploc = double(2), indicator = double(0), log = integer(0, default = 0)) {
+    if(indicator == 0) { if(sum(x) == 0) return(0) else return(-Inf) }
+    lp <- 0
+    xInd <- rep(1, nocc)
+    for(s in 1:ntrap) {
+      dist <- (SX-traploc[s,1])^2 + (SY-traploc[s,2])^2
+      for(j in 1:nocc) {
+        p <- max(min(pbar[s,j]*exp(-dist/(sigma)),0.999),0.001)*eff.ind[s,j]
+        if(x[j,xInd[j]] == s) {
+          lp <- lp + log(p)
+          if(xInd[j] < MAX) xInd[j] <- xInd[j] + 1
+        } else {
+          lp <- lp + log(1-p)
+        }
+      }
+    }
+    returnType(double())
+    if(log) return(lp)
+    return(exp(lp))
+  })
+
+rBernoulliVector4 <- nimbleFunction(
+  run = function(
+    n = integer(), MAX = double(0), ntrap = double(0), nocc = double(0),
+    sigma = double(0), pbar = double(2), eff.ind = double(2), SX = double(0), SY = double(0),
+    traploc = double(2), indicator = double(0)) {
+    stop('should never call rBernoulliVector4')
+    returnType(double(2))
+    return(array(0, c(2, 2)))
+  })
+
+registerDistributions(list(
+  dBernoulliVector4 = list(
+    BUGSdist = 'dBernoulliVector4(MAX,ntrap,nocc,sigma,pbar,eff.ind,SX,SY,traploc,indicator)',
+    types = c('value = double(2)', 'sigma = double(0)', 'pbar = double(2)','eff.ind = double(2)', 'traploc = double(2)'),
+    mixedSizes = TRUE   ## DT: prevents a warning about size/dimension mismatch
+  )))
+# ---- Build data ----
+
+# constants
+constants <-  list(nsites = datSCR$nsites, nocc = datSCR$nocc, M = datSCR$M, ntrap = datSCR$ntrap,
+                   traploc = datSCR$traploc, habitat = datSCR$habitat,
+                   seff = datSCR$seff, eff.ind = datSCR$eff.ind,
+                   MAX = datSCR$MAX)  
+
+# data
+data <- list(sites= datSCR$sites, y.scr = datSCR$y.scr, zeros = rep(0,M))
+
+# Inits
+inits <-  list(z=datSCR$z,id = datSCR$id, mu0=0,mu1=0,sigma = runif(1,10^2,10^5), p0 = 0, p1= 0, psi = runif(1,0,1))
+
+# NIMBLE RUN ----
+
+  # Build model
+Rmodel <- nimbleModel(SCRcode, constants, data, inits)
+ Rmodel$calculate() # - 262110
+# configure model
+conf <- configureMCMC(Rmodel)
+    
+  conf$printMonitors()
+  conf$resetMonitors()
+  conf$addMonitors(c('N','EN','mu0','mu1',"sigma", "p0","p1", "id", "z"))
+  
+  # custom samplers OPTIONNAL
+  conf$printSamplers(byType= TRUE)
+  
+ conf$removeSamplers(c("mu0","mu1","p0","p1"))
+ 
+  conf$addSampler(c("mu0","mu1"), type ="RW_block")
+  conf$addSampler(c("p0","p1"), type ="RW_block")
+  conf$printSamplers(byType= TRUE)
+  
+  conf$removeSamplers('id')
+  conf$addSampler('id', type = 'RW', scalarComponents = TRUE,
+                  control = list(adaptive = FALSE, reflective = TRUE, scale = datSCR$nsites/3)) ## DT
+  
+  
+  # Build and compile MCMC
+  Rmcmc <- buildMCMC(conf)
+  Cmodel <- compileNimble(Rmodel)
+  Cmcmc <- compileNimble(Rmcmc, project = Cmodel)
+
+  # Run
+  # ART: 11 h 
+t <- system.time(samples <- runMCMC(Cmcmc, niter = 50000, nburnin = 5000, nchains = 2, thin = 10, samplesAsCodaMCMC = TRUE) ) ## DT: use runMCMC
+
+# ART : 6 min
+t <- system.time(samples <- runMCMC(Cmcmc, niter = 1000, nburnin = 100, nchains = 2, samplesAsCodaMCMC = TRUE) ) ## DT: use runMCMC
+
+niter_ad <- 6000
+CsurvivalMCMC$run(niter_ad, reset = FALSE)
+
+traplot(samples[,c("EN","mu0","mu1","sigma.ds","sigma.scr")])
+save(samples,t, file ="SCR_sig0res2.Rdata")
+
+# mcmcplots::denplot(samples[,c('mu0',"mu1")])
+# 
+# quantile(ch[,"mu0"])
+# quantile(ch[,"mu1"])
+# # plot map density 
+# 
+# head(ch)
+# dim(ch)
+# 
+# ch1 <- samples
+# 
+# SCRch <- list(as.mcmc(ch1))
+# 
+# SCR <- coda::as.mcmc.list(SCRch)
+# 
+#               
+# ch <- ch[sample(1:nrow(ch),1000),]
+# sigmascr <- matrix(NA,nrow= nrow(ch),ncol = length(datSCR$seff[datSCR$seff>0]))
+# res2 <- matrix(NA,nrow= nrow(ch),ncol = nrow(sea$bathy.sc))
+# p0scr_scr <- matrix(NA,nrow= nrow(ch),ncol = length(datSCR$seff[datSCR$seff>0]))
+# 
+# mu0scr <- mu1scr <- rep(NA,nrow(ch))
+# beta0scr <- beta1scr <- rep(NA,nrow(ch))
+# p0scr <- p1scr <- rep(NA,nrow(ch))
+# 
+# for(i in 1:nrow(res2)){
+#   res2[i,] <- exp(ch[i,"mu0"]+ ch[i,"mu1"]*sea$bathy.sc)
+#   mu0scr[i] <- ch[i,"mu0"]
+#   mu1scr[i] <- ch[i,"mu1"]
+#   
+#   beta0scr[i] <- ch[i,"beta0"]
+#   beta1scr[i] <- ch[i,"beta1"]
+#   sigmascr[i,] <- exp(beta0scr[i] + beta1scr[i]*datSCR$seff[datSCR$seff>0])
+#   
+#   p0scr[i] <- ch[i,"p0"]
+#   p1scr[i] <- ch[i,"p1"]
+#   
+#   p0scr_scr[i,] <-  exp(p0scr[i] + p1scr[i]*datSCR$seff[datSCR$seff>0])
+# }
+# 
+# resSCR <- apply(res2,1,sum)
+# summary(resSCR)
+# 
+# map2 <- ggplot()+ geom_sf(data = sea, aes(fill = matrixStats::colMedians(res2)), lwd = 0.01) + geom_sf(data = pays, lwd = 0.2) + 
+#   scale_fill_viridis_c(limits = c(0,max(matrixStats::colMedians(res2)))) +
+#   labs(title = "Spatial Capture-Recapture",fill = "Density")+ theme_cowplot(rel_large = 1, rel_tiny = 0.7, font_family = "Montserrat")
+# 
+# map2
+# 
+# # try to plot ACs
+# # 1 chain MCMC
+# head(samples[,5000:5008])
+# 
+# ids <- round(samples[,5:5004])
+# tabid <- table(ids)
+# 
+# 
+# mapAC <- ggplot() + 
+#   geom_sf(data = sea, aes(fill= tabid),lwd = 0) + 
+#   scale_fill_viridis_c() +
+#   geom_sf(data= pays)
+# 
+# mapAC
+
